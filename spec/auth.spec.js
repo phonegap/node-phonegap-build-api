f9ec78f2c16aef9765eb4d81988d857b4544c6ce
Michael Brooks <michael@michaelbrooks.ca>
2012-10-30 11:44:56 -0700

    Mock requests in auth spec.

--- spec/auth.spec.js
+++ spec/auth.spec.js
@@ -1,13 +1,17 @@
 var api      = require('../lib/main'),
     Api      = require('../lib/api'),
     request  = require('request'),
-    username = 'filmaj@apache.org',
-    password = 'apache';
+    username = 'filmaj@apache.org', // real username for PhoneGap Build
+    password = 'apache';            // real password for PhoneGap Build
 
 describe('auth', function() {
     describe('incorrect username', function() {
         it('should report an error in the callback', function() {
             var s = jasmine.createSpy();
+            spyOn(request, 'post').andCallFake(function(options, callback) {
+                callback({}, { 'statusCode': 404 }, '{}');
+            });
+
             runs(function() {
                 api.auth('raoul_duke@fearandloathing.com', 'balls', s);
             });
@@ -21,6 +25,10 @@ describe('auth', function() {
     describe('incorrect password', function() {
         it('should report an error in the callback', function() {
             var s = jasmine.createSpy();
+            spyOn(request, 'post').andCallFake(function(options, callback) {
+                callback({}, { 'statusCode': 404 }, '{}');
+            });
+
             runs(function() {
                 api.auth(username, 'balls', s);
             });
@@ -34,6 +42,10 @@ describe('auth', function() {
     describe('correct username and password', function() {
         it('should return an API object with a token', function() {
             var s = jasmine.createSpy();
+            spyOn(request, 'post').andCallFake(function(options, callback) {
+                callback({}, { 'statusCode': 200 }, '{}');
+            });
+
             runs(function() {
                 api.auth(username, password, s);
             });
