d694b386bcafd463ceee186cb40e6ed2ccac70bd
Michael Brooks <michael@michaelbrooks.ca>
2012-10-31 09:43:30 -0700

    Update API token spec.

--- spec/api.spec.js
+++ spec/api.spec.js
@@ -1,14 +1,14 @@
 var Api = require('../lib/api');
 
 describe('Api', function() {
-    describe('token property', function() {
+    describe('token', function() {
         it('should be set with constructor', function() {
             var token = Math.random().toString();
-            var api = new Api({'token': token});
+            var api = new Api({ 'token': token });
             expect(api.token).toEqual(token);
         });
 
-        it('should throw an error when missing', function() {
+        it('should throw error when missing', function() {
             expect(function() {
                 var api = new Api();
             }).toThrow();
