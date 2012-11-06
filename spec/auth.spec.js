da82886d75e50daa266ef479c08bdbad53892ba3
Michael Brooks <michael@michaelbrooks.ca>
2012-11-05 16:36:23 -0800

    Refactor auth specs for clarity.

--- spec/auth.spec.js
+++ spec/auth.spec.js
@@ -11,46 +11,49 @@ describe('auth', function() {
         };
     });
 
-    describe('incorrect username', function() {
-        it('should return an error in the callback', function(done) {
-            // Mock invalid username response
+    describe('invalid', function() {
+        beforeEach(function() {
+            // Mock invalid username or password response
             spyOn(request, 'post').andCallFake(function(options, callback) {
                 callback({}, { 'statusCode': 401 }, '{"error":"Invalid email or password."}');
             });
+        });
 
-            client.auth(options, function(e, data) {
-                expect(e).toEqual(jasmine.any(Object));
-                expect(data).toBeNull();
-                done();
+        describe('user', function() {
+            it('should return an error in the callback', function(done) {
+                client.auth(options, function(e, data) {
+                    expect(e).toEqual(jasmine.any(Object));
+                    expect(data).toBeNull();
+                    done();
+                });
+            });
+        });
+
+        describe('password', function() {
+            it('should report an error in the callback', function(done) {
+                client.auth(options, function(e, data) {
+                    expect(e).toEqual(jasmine.any(Object));
+                    expect(data).toBeNull();
+                    done();
+                });
             });
         });
     });
 
-    describe('incorrect password', function() {
-        it('should report an error in the callback', function(done) {
-            // Mock invalid password response
-            spyOn(request, 'post').andCallFake(function(options, callback) {
-                callback({}, { 'statusCode': 401 }, '{"error":"Invalid email or password."}');
-            });
-
-            client.auth(options, function(e, data) {
-                expect(e).toEqual(jasmine.any(Object));
-                expect(data).toBeNull();
-                done();
-            });
-        });
-    });
-
-    describe('correct username and password', function() {
-        it('should return an API object with a token', function() {
+    describe('valid', function() {
+        beforeEach(function() {
             // Mock valid auth response
             spyOn(request, 'post').andCallFake(function(options, callback) {
                 callback({}, { 'statusCode': 200 }, '{"token":"Y9nGxwX7QenyuNXSaEnp"}');
             });
+        });
 
-            client.auth(options, function(e, data) {
-                expect(e).toBeNull();
-                expect(data).toEqual(jasmine.any(Function));
+        describe('username and password', function() {
+            it('should return an API object with a token', function() {
+                client.auth(options, function(e, data) {
+                    expect(e).toBeNull();
+                    expect(data).toEqual(jasmine.any(Function));
+                });
             });
         });
     });
