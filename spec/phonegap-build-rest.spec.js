d1ee97993929f5001c58be952dcbbf94e0611528
Michael Brooks <michael@michaelbrooks.ca>
2012-12-19 11:24:48 -0600

    [spec] Rewrite auth.spec as phonegap-build-rest.spec

---
+++ spec/phonegap-build-rest.spec.js
@@ -0,0 +1,141 @@
+/*
+ * Module dependencies.
+ */
+
+var client = require('../lib/client'),
+    request = require('request');
+
+/*
+ * Specification for phonegap-build-rest client.
+ */
+
+describe('phonegap-build-rest', function() {
+    describe('auth(options, callback)', function() {
+        var options;
+        beforeEach(function() {
+            options = { username: 'zelda', password: 'tr1force' };
+        });
+
+        it('should be a function', function() {
+            expect(client.auth).toEqual(jasmine.any(Function));
+        });
+
+        it('should require options.username', function() {
+            expect(function() {
+                options.username = undefined;
+                client.auth(options, function(e, api) {});
+            }).toThrow();
+        });
+
+        it('should require options.password', function() {
+            expect(function() {
+                options.password = undefined;
+                client.auth(options, function(e, api) {});
+            }).toThrow();
+        });
+
+        it('should require callback', function() {
+            expect(function() {
+                client.auth(options, null);
+            }).toThrow();
+        });
+
+        it('should try to authenticate with server', function() {
+            spyOn(request, 'post');
+            client.auth(options, function(e, api) {});
+            expect(request.post).toHaveBeenCalledWith(
+                'https://build.phonegap.com:443/token',
+                jasmine.any(Object),
+                jasmine.any(Function)
+            );
+        });
+
+        describe('successful authentication', function() {
+            beforeEach(function() {
+                spyOn(request, 'post').andCallFake(function(uri, opts, callback) {
+                    callback(null, { statusCode: 200 }, '{ "token": "abc123" }');
+                });
+            });
+
+            it('should trigger callback without an error', function(done) {
+                client.auth(options, function(e, api) {
+                    expect(e).toBeNull();
+                    done();
+                });
+            });
+
+            it('should trigger callback with an API object', function(done) {
+                client.auth(options, function(e, api) {
+                    expect(api).toBeDefined();
+                    done();
+                });
+            });
+        });
+
+        describe('failed authentication request', function() {
+            beforeEach(function() {
+                spyOn(request, 'post').andCallFake(function(uri, opts, callback) {
+                    callback(new Error('request timeout'), {}, '');
+                });
+            });
+
+            it('should trigger callback with an error', function(done) {
+                client.auth(options, function(e, api) {
+                    expect(e).toEqual(jasmine.any(Error));
+                    done();
+                });
+            });
+
+            it('should trigger callback without an API object', function(done) {
+                client.auth(options, function(e, api) {
+                    expect(api).not.toBeDefined();
+                    done();
+                });
+            });
+        });
+
+        describe('failed authentication response', function() {
+            beforeEach(function() {
+                spyOn(request, 'post').andCallFake(function(uri, opts, callback) {
+                    callback(null, { statusCode: 404 }, '');
+                });
+            });
+
+            it('should trigger callback with an error', function(done) {
+                client.auth(options, function(e, api) {
+                    expect(e).toEqual(jasmine.any(Error));
+                    done();
+                });
+            });
+
+            it('should trigger callback without an API object', function(done) {
+                client.auth(options, function(e, api) {
+                    expect(api).not.toBeDefined();
+                    done();
+                });
+            });
+        });
+
+        describe('failed authentication credentials', function() {
+            beforeEach(function() {
+                spyOn(request, 'post').andCallFake(function(uri, opts, callback) {
+                    callback(null, { statusCode: 200 }, ' { "error": "incorrect password" }');
+                });
+            });
+
+            it('should trigger callback with an error', function(done) {
+                client.auth(options, function(e, api) {
+                    expect(e).toEqual(jasmine.any(Error));
+                    done();
+                });
+            });
+
+            it('should trigger callback without an API object', function(done) {
+                client.auth(options, function(e, api) {
+                    expect(api).not.toBeDefined();
+                    done();
+                });
+            });
+        });
+    });
+});
